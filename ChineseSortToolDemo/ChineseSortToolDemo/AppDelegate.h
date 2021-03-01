3e4cd339f456a4de1e5ed24af1c1aafc68406084
xiehaiyan <1170068445@qq.com>
2021-03-01 15:06:38 +0800

    update code

---
+++ ChineseSortToolDemo/ChineseSortToolDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChineseSortToolDemo
+//
+//  Created by xiehy on 2021/3/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
